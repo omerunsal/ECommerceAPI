cc88330b5d3cf5aef6f67e3f7ef889a6e6a82007
omerunsal <47754244+omerunsal@users.noreply.github.com>
2023-10-12 15:21:59 +0300

    dummy data test

---
+++ ECommerceAPI/Infrastructure/ECommerceAPI.Persistence/Concrete/ProductService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerceAPI.Application.Abstract;
+using ECommerceAPI.Domain.Entities;
+
+namespace ECommerceAPI.Persistence.Concrete
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+            => new()
+            {
+                new() { Id=Guid.NewGuid(), Name="Product1", Price = 100, Stock = 10},
+                new() { Id=Guid.NewGuid(), Name="Product2", Price = 100, Stock = 10},
+                new() { Id=Guid.NewGuid(), Name="Product3", Price = 100, Stock = 10},
+                new() { Id=Guid.NewGuid(), Name="Product4", Price = 100, Stock = 10}
+            };
+    }
+}
---
+++ ECommerceAPI/Infrastructure/ECommerceAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerceAPI.Application.Abstract;
+using ECommerceAPI.Persistence.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerceAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
